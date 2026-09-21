a86385416a4bcfee7fde199851266958d31fd669
616042024@qq.com <616042024@qq.com>
2026-09-21 10:15:54 +0800

    Add Ctrl right-click quick selling for backpack items

--- src/inventory-drag.ts
+++ src/inventory-drag.ts
@@ -24,6 +24,7 @@ type Drag = {
 export class InventoryDrag {
   private ui: UI;
   private drag?: Drag;
+  get isDragging() { return !!this.drag; }
   private frame = 0;
   private lastFrame = 0;
   private suppressClick = false;
